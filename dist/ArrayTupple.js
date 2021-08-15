72a53f665c914e491759aa7085be66baeb8f4ebb
Nitesh Sharma <nitesh.grep@gmail.com>
2021-08-15 14:13:42 +0530

    Tupple and Arrays

---
+++ dist/ArrayTupple.js
@@ -0,0 +1,3 @@
+let car = [2002, 'Skoda', 'Superb'];
+// Nothing is stopping you to do this.
+car.push('SomethingElse');
---
+++ src/ArrayTupple.ts
@@ -0,0 +1,4 @@
+let car: [number, string, string] = [2002, 'Skoda', 'Superb']
+
+// Nothing is stopping you to do this.
+car.push('SomethingElse')
