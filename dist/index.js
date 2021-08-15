d157143266c2bb3b878a33bd4c7a0a27d9d8cdca
Nitesh Sharma <nitesh.grep@gmail.com>
2021-08-15 11:10:51 +0530

    Setting up basic typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+async function timeOut(number) {
+    return number;
+}
+function add(a, b) {
+    return a + b;
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+async function timeOut(number: number) {
+  return number
+}
+
+function add(a: number, b: number) {
+  return a + b;
+}
