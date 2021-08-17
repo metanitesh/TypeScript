a48c629d568c1d3e55e155a7ecf309e84a6360e3
Nitesh Sharma <nitesh.grep@gmail.com>
2021-08-17 08:40:18 +0530

    Types Exercise

---
+++ src/09.typesExercise.ts
@@ -0,0 +1,32 @@
+type Primitive = string | number | boolean | null
+type JSONObject = {
+  [key: string]: JSONValue
+}
+type JSONArray = JSONValue[]
+type JSONValue = Primitive | JSONArray | JSONObject
+
+////// DO NOT EDIT ANY CODE BELOW THIS LINE //////
+function isJSON(arg: JSONValue) { }
+
+// POSITIVE test cases (must pass)
+isJSON("hello")
+isJSON([4, 8, 15, 16, 23, 42])
+isJSON({ greeting: "hello" })
+isJSON(false)
+isJSON(true)
+isJSON(null)
+isJSON({
+  a:
+    { b: [2, 3, "foo"] }
+})
+
+
+isJSON(() => "")
+
+isJSON(class { })
+
+isJSON(undefined)
+
+isJSON(new BigInt(143))
+
+isJSON(isJSON)
